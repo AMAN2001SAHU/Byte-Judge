35c1453b65cc34eeedbbdf57bd61b18929b5fc14
Aman Sahu <aman.sahu@juspay.in>
2025-12-07 02:09:48 +0530

    Test done reverting changes

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,7 +1,7 @@
 // import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import './index.css'; 
+import './index.css';
 import { UIProvider } from './context/UIProvider.tsx';
 import AppRoutes from './routes/AppRoutes.tsx';
 
